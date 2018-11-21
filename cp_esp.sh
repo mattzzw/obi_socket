5255e03f15df49adf1793106f5ba54397292fb76
Matthias Wientapper <m.wientapper@gmx.de>
2018-11-21 18:57:59 +0100

    New micropython firmware with umqtt.robust and utemplate, added css framework

--- cp_esp.sh
+++ cp_esp.sh
@@ -3,12 +3,13 @@
 # Copies files in via webrepl_cli tool to esp8266
 
 WEBREPL_CLI=../../esp8266/webrepl/webrepl_cli.py
-PASSWD=Secret
+PASSWD=Secrect
 
-IP=192.168.4.1
+#IP=192.168.4.1
+IP=obi-socket1
 
 for file in $@
 do
     echo $file
-    $WEBREPL_CLI -p $PASSWD $file $IP:/
+    $WEBREPL_CLI -p $PASSWD $file $IP:/$file
 done
